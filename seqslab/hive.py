50ca1d8ad39b62e8e37ddec68cf77f348a5849ff
apple <allen.chang@atgenomix.com>
2023-05-22 15:37:21 +0800

    DBAPI with pyhive

---
+++ seqslab/hive.py
@@ -0,0 +1,9 @@
+from pyhive import hive
+
+# PEP 249 module globals
+apilevel = '3.0'
+threadsafety = 2  # Threads may share the module and connections.
+paramstyle = 'pyformat'  # Python extended format codes, e.g. ...WHERE name=%(name)s
+
+
+
