d6cd15ceac6652dadc672cf0fcc5537131b4ac38
Yuting LIN <johnny5584767@gmail.com>
2023-05-24 15:53:39 +0800

    ignore case which col_type is None when get_columns

--- seqslab/sqlalchemy/hive.py
+++ seqslab/sqlalchemy/hive.py
@@ -22,7 +22,9 @@ SQLAlchemy dialect for seqslab.hive
 import re
 
 from seqslab import hive
-from pyhive.sqlalchemy_hive import HiveDialect
+from pyhive.sqlalchemy_hive import HiveDialect, _type_map
+from sqlalchemy import types
+from sqlalchemy import util
 
 
 class SeqsLabHiveDialect(HiveDialect):
@@ -58,3 +60,33 @@ class SeqsLabHiveDialect(HiveDialect):
         if schema:
             query += ' IN ' + self.identifier_preparer.quote_identifier(schema)
         return [row[1] for row in connection.execute(query)]
+
+    def get_columns(self, connection, table_name, schema=None, **kw):
+        rows = self._get_table_columns(connection, table_name, schema)
+        # Strip whitespace
+        rows = [[col.strip() if col else None for col in row] for row in rows]
+        # Filter out empty rows and comment
+        rows = [row for row in rows if row[0] and row[0] != '# col_name']
+        result = []
+        for (col_name, col_type, _comment) in rows:
+            if col_type is None:
+                continue
+            if col_name == '# Partition Information':
+                break
+            # Take out the more detailed type information
+            # e.g. 'map<int,int>' -> 'map'
+            #      'decimal(10,1)' -> decimal
+            col_type = re.search(r'^\w+', col_type).group(0)
+            try:
+                coltype = _type_map[col_type]
+            except KeyError:
+                util.warn("Did not recognize type '%s' of column '%s'" % (col_type, col_name))
+                coltype = types.NullType
+
+            result.append({
+                'name': col_name,
+                'type': coltype,
+                'nullable': True,
+                'default': None,
+            })
+        return result
